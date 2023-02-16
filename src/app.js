f392c39bbf325aa883a803df285a6f9459bd075a
lev <levovskiiy@yandex.ru>
2023-02-16 19:32:42 +0300

    feat: добавил ограничение запросов

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import express from 'express';
 import routes from './routes/index.js';
-import { errorsHandler, corsHandler, logger } from './middlewares/index.js';
+import { errorsHandler, corsHandler, logger, rateLimit } from './middlewares/index.js';
 
 const app = express();
 app.use(helmet());
@@ -12,6 +12,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(corsHandler());
+app.use(rateLimit);
 app.use(logger.requestLogger);
 app.use(routes);
 app.use(logger.errorLogger);
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,8 +2,9 @@ import * as logger from './logger.js';
 import authentication from './auth.js';
 import corsHandler from './cors.js';
 import errorsHandler from './errors.js';
+import rateLimit from './rateLimit.js';
 import * as validators from './validators.js';
 
 export {
-  logger, authentication, corsHandler, errorsHandler, validators,
+  logger, authentication, corsHandler, errorsHandler, validators, rateLimit
 };
